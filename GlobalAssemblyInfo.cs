f311eb5a75f757afe9ada8c720bb36861ee9c92f
michael dudley <michael.dudley@improvingenterprises.com>
2017-01-02 12:13:34 -0600

    3.0.1

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -7,6 +7,6 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
+[assembly: AssemblyInformationalVersion("3.0.1.0")]
